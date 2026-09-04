92635946ab2cb8e5745713be7a8ea5e45a822886
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-09-04 21:08:06 +0500

    add habit, habitlog models

---
+++ apps/habits/models/habit_log.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models import UniqueConstraint
+
+from .habit import Habit
+
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name="logs")
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.habit} - {self.date}"
+
+    class Meta:
+        db_table = "habit_log"
+        verbose_name = "Habit log"
+        verbose_name_plural = "Habit logs"
+        constraints = [UniqueConstraint(fields=["habit", "date"], name="unique_habit_log")]
+
